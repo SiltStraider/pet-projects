aa07948a0b109e30cb11450e83ed71b1a4c842dc
SiltStraider <lesha.vinokurovv@yandex.ru>
2022-12-28 20:40:32 +0500

    accordion-guests+tabs

--- js/accordion-guests.js
+++ js/accordion-guests.js
@@ -1,4 +1,4 @@
-class ItcAccordion {
+class ItcAccordion2 {
   constructor(target, config) {
     this._el = typeof target === 'string' ? document.querySelector(target) : target;
     const defaultConfig = {
@@ -9,19 +9,19 @@ class ItcAccordion {
   }
   addEventListener() {
     this._el.addEventListener('click', (e) => {
-      const elHeader = e.target.closest('.accordion__header_header');
+      const elHeader = e.target.closest('.guests-accordion__top-content');
       if (!elHeader) {
         return;
       }
       if (!this._config.alwaysOpen) {
-        const elOpenItem = this._el.querySelector('.accordion__item_header_show');
+        const elOpenItem = this._el.querySelector('.guests-accordion__item_show');
         if (elOpenItem) {
-          elOpenItem !== elHeader.parentElement ? elOpenItem.classList.toggle('accordion__item_header_show') : null;
+          elOpenItem !== elHeader.parentElement ? elOpenItem.classList.toggle('guests-accordion__item_show') : null;
         }
       }
-      elHeader.parentElement.classList.toggle('accordion__item_header_show');
+      elHeader.parentElement.classList.toggle('guests-accordion__item_show');
     });
   }
 }
 
-new ItcAccordion('#accordion-1');
+new ItcAccordion2('#accordion-2');
